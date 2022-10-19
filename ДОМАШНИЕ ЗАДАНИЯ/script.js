b4d5af0925b0bb6e44cfe4ae47d4a8b9486b9306
NataManatu <vip.aleksa78@mail.ru>
2022-10-19 19:42:58 +0300

    Js

--- ДОМАШНИЕ ЗАДАНИЯ/script.js
+++ ДОМАШНИЕ ЗАДАНИЯ/script.js
@@ -11,35 +11,33 @@ hamlet.gif (файл прикреплен к данному pdf-файлу). */
 
 // let str2 = "<br>William Shakespeare, from 'Hamlet'"
 
-// for (let i = 0; i < str.length; i++) {
-//     setTimeout(function () {
-//         let element = str[i];
-//         document.write(element)
-//     }, 3000)
+// for (let i = 0; i < str.length; i++) {setTimeout(()=>{document.write(str[i])}, i*200)}
 
-// }
-// for (let i = 0; i < str2.length; i++) {
-//     setTimeout(function () {
-//         let element = str2[i];
-//         document.write(element)
-//     }, 4000)
 
+// setTimeout(secondStr, str.length*200+500)
+
+// function secondStr() {
+//     for (let i = 0; i < str2.length; i++) {
+//         setTimeout(function () {
+//             document.write(str2[i])
+//         }, i*200)
+//     }
 // }
 
 /* Задание 2
 Рассчитайте, сколько дней, часов, минут и секунд
 осталось до Нового года. Выведите эти значения красиво, используя метод document.write() с тегами <p> и
 <span> и классами для них. Стили можно записать в отдельном css-файле. Если одно из чисел будет меньше 10,
-то его нужно вывести с ведущим 9.Сделайте свой скрипт универсальным, задав дату следующего Нового года 
+то его нужно вывести с ведущим 0.Сделайте свой скрипт универсальным, задав дату следующего Нового года 
 относительно текущей даты с помощью методов объекта Date.  */
 
 
 today = new Date()
 newYear = new Date("January 1, 2023")
-getDays = Math.round((newYear - today) / 24 / 60 / 60 / 1000);
-getHours = Math.round((newYear - today) / 60 / 60 / 1000);
-getMin = Math.round((newYear - today) / 60 / 1000);
+// getDays = Math.round((newYear - today) / 24 / 60 / 60 / 1000);
+// getHours = Math.round((newYear - today) / 60 / 60 / 1000);
+// getMin = Math.round((newYear - today) / 60 / 1000);
 getSecond = Math.round((newYear - today) / 1000);
-console.log(getHours)
-console.log(getSecond)
-document.write(`<p>До Нового года осталось</p><br> <span>${getDays}</span>  <span>${getHours}</span>  <span>${getMin}</span>  <span>${getSecond}</span><br><br> <span id="span">дня</span> <span id="span">часа</span> <span id="span">минут</span> <span id="span">секунд</span> `) 
+// console.log(getHours)
+// console.log(getSecond)
+document.write(`<p>До Нового года осталось</p><br> <span>${Math.floor(getSecond/24/60/60%365)}</span>  <span>${Math.floor(getSecond/60/60%24)}</span>  <span>${Math.floor(getSecond/60%60)}</span>  <span>${Math.floor(getSecond%60)}</span><br><br> <span id="span">дня</span> <span id="span">часа</span> <span id="span">минут</span> <span id="span">секунд</span> `) 
